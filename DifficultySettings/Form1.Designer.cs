3f9a8fc83b578894169f092fa0c30bba0d0c0427
GradyRuefferOutlook <gradruef152@outlook.com>
2022-09-22 11:28:08 -0400

    Add project files.

---
+++ DifficultySettings/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace DifficultySettings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.difficultyLabel = new System.Windows.Forms.Label();
+            this.easyButton = new System.Windows.Forms.Button();
+            this.normalButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.masterButton = new System.Windows.Forms.Button();
+            this.beginButton = new System.Windows.Forms.Button();
+            this.bButton = new System.Windows.Forms.Button();
+            this.aButton = new System.Windows.Forms.Button();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.nameLabel.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nameLabel.Location = new System.Drawing.Point(13, 13);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(419, 58);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "WELCOME YONDER TRAVELLER! WHAT IS THY NAME?";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nameInput
+            // 
+            this.nameInput.Font = new System.Drawing.Font("Press Start 2P", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameInput.Location = new System.Drawing.Point(272, 74);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(250, 26);
+            this.nameInput.TabIndex = 1;
+            // 
+            // difficultyLabel
+            // 
+            this.difficultyLabel.Font = new System.Drawing.Font("Press Start 2P", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.difficultyLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.difficultyLabel.Location = new System.Drawing.Point(13, 118);
+            this.difficultyLabel.Name = "difficultyLabel";
+            this.difficultyLabel.Size = new System.Drawing.Size(509, 23);
+            this.difficultyLabel.TabIndex = 2;
+            this.difficultyLabel.Text = "HOW DIFFICULT IS THOUST QUEST?";
+            this.difficultyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // easyButton
+            // 
+            this.easyButton.BackColor = System.Drawing.Color.Black;
+            this.easyButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.easyButton.ForeColor = System.Drawing.Color.White;
+            this.easyButton.Location = new System.Drawing.Point(17, 263);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(118, 62);
+            this.easyButton.TabIndex = 3;
+            this.easyButton.Text = "Easy";
+            this.easyButton.UseVisualStyleBackColor = false;
+            this.easyButton.Click += new System.EventHandler(this.easyButton_Click);
+            // 
+            // normalButton
+            // 
+            this.normalButton.BackColor = System.Drawing.Color.Black;
+            this.normalButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.normalButton.ForeColor = System.Drawing.Color.White;
+            this.normalButton.Location = new System.Drawing.Point(214, 144);
+            this.normalButton.Name = "normalButton";
+            this.normalButton.Size = new System.Drawing.Size(118, 62);
+            this.normalButton.TabIndex = 4;
+            this.normalButton.Text = "Normal";
+            this.normalButton.UseVisualStyleBackColor = false;
+            // 
+            // hardButton
+            // 
+            this.hardButton.BackColor = System.Drawing.Color.Black;
+            this.hardButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hardButton.ForeColor = System.Drawing.Color.White;
+            this.hardButton.Location = new System.Drawing.Point(404, 263);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(118, 62);
+            this.hardButton.TabIndex = 5;
+            this.hardButton.Text = "Hard";
+            this.hardButton.UseVisualStyleBackColor = false;
+            // 
+            // masterButton
+            // 
+            this.masterButton.BackColor = System.Drawing.Color.Black;
+            this.masterButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masterButton.ForeColor = System.Drawing.Color.White;
+            this.masterButton.Location = new System.Drawing.Point(214, 393);
+            this.masterButton.Name = "masterButton";
+            this.masterButton.Size = new System.Drawing.Size(118, 62);
+            this.masterButton.TabIndex = 6;
+            this.masterButton.Text = "Master";
+            this.masterButton.UseVisualStyleBackColor = false;
+            // 
+            // beginButton
+            // 
+            this.beginButton.BackColor = System.Drawing.Color.Black;
+            this.beginButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beginButton.ForeColor = System.Drawing.Color.White;
+            this.beginButton.Location = new System.Drawing.Point(214, 263);
+            this.beginButton.Name = "beginButton";
+            this.beginButton.Size = new System.Drawing.Size(118, 62);
+            this.beginButton.TabIndex = 7;
+            this.beginButton.Text = "Begin";
+            this.beginButton.UseVisualStyleBackColor = false;
+            // 
+            // bButton
+            // 
+            this.bButton.BackColor = System.Drawing.Color.Black;
+            this.bButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bButton.ForeColor = System.Drawing.Color.White;
+            this.bButton.Location = new System.Drawing.Point(422, 450);
+            this.bButton.Name = "bButton";
+            this.bButton.Size = new System.Drawing.Size(118, 62);
+            this.bButton.TabIndex = 8;
+            this.bButton.Text = "B";
+            this.bButton.UseVisualStyleBackColor = false;
+            // 
+            // aButton
+            // 
+            this.aButton.BackColor = System.Drawing.Color.Black;
+            this.aButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aButton.ForeColor = System.Drawing.Color.White;
+            this.aButton.Location = new System.Drawing.Point(-3, 450);
+            this.aButton.Name = "aButton";
+            this.aButton.Size = new System.Drawing.Size(118, 62);
+            this.aButton.TabIndex = 9;
+            this.aButton.Text = "A";
+            this.aButton.UseVisualStyleBackColor = false;
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.Font = new System.Drawing.Font("Press Start 2P", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.welcomeLabel.Location = new System.Drawing.Point(121, 479);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(295, 23);
+            this.welcomeLabel.TabIndex = 10;
+            this.welcomeLabel.Text = "Welcome";
+            this.welcomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.welcomeLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.aButton);
+            this.Controls.Add(this.bButton);
+            this.Controls.Add(this.beginButton);
+            this.Controls.Add(this.masterButton);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.normalButton);
+            this.Controls.Add(this.easyButton);
+            this.Controls.Add(this.difficultyLabel);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.nameLabel);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Label difficultyLabel;
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Button normalButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.Button masterButton;
+        private System.Windows.Forms.Button beginButton;
+        private System.Windows.Forms.Button bButton;
+        private System.Windows.Forms.Button aButton;
+        private System.Windows.Forms.Label welcomeLabel;
+    }
+}
+
---
+++ DifficultySettings/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DifficultySettings
+{
+    public partial class Form1 : Form
+    {
+        int babyCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void easyButton_Click(object sender, EventArgs e)
+        {
+            if (babyCounter == 10)
+            {
+                difficultyLabel.Text = "Wittle Baby Needs His Easy Mode?";
+                easyButton.BackColor = Color.LightBlue;
+                normalButton.BackColor = Color.LightBlue;
+                hardButton.BackColor = Color.LightBlue;
+                masterButton.BackColor = Color.LightBlue;
+                aButton.BackColor = Color.LightBlue;
+                bButton.BackColor = Color.LightBlue;
+                easyButton.Text = "Baby";
+                normalButton.Text = "Baby";
+                hardButton.Text = "Baby";
+                masterButton.Text = "Baby";
+                aButton.Text = "Baby";
+                bButton.Text = "Baby";
+            } else
+            {
+                setToBlack();
+                easyButton.BackColor = Color.Green;
+                difficultyLabel.Text = "Really?";
+                babyCounter++;
+            }
+        }
+        void setToBlack()
+        {
+            easyButton.BackColor = Color.Black;
+            normalButton.BackColor = Color.Black;
+            hardButton.BackColor = Color.Black;
+            masterButton.BackColor = Color.Black;
+            aButton.BackColor = Color.Black;
+            bButton.BackColor = Color.Black;
+        }
+    }
+}
