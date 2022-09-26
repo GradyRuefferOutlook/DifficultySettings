c7bb904e43c9d680cbb62fdd94240c5bcaa2f977
Grady-Rueffer <94403129+Grady-Rueffer@users.noreply.github.com>
2022-09-26 00:53:51 -0400

    Finished code

--- DifficultySettings/Form1.Designer.cs
+++ DifficultySettings/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.nameLabel = new System.Windows.Forms.Label();
             this.nameInput = new System.Windows.Forms.TextBox();
             this.difficultyLabel = new System.Windows.Forms.Label();
@@ -39,12 +40,13 @@
             this.bButton = new System.Windows.Forms.Button();
             this.aButton = new System.Windows.Forms.Button();
             this.welcomeLabel = new System.Windows.Forms.Label();
+            this.nameChecker = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // nameLabel
             // 
             this.nameLabel.BackColor = System.Drawing.Color.Transparent;
-            this.nameLabel.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.nameLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.nameLabel.Location = new System.Drawing.Point(13, 13);
             this.nameLabel.Name = "nameLabel";
@@ -55,15 +57,15 @@
             // 
             // nameInput
             // 
-            this.nameInput.Font = new System.Drawing.Font("Press Start 2P", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.nameInput.Location = new System.Drawing.Point(272, 74);
             this.nameInput.Name = "nameInput";
-            this.nameInput.Size = new System.Drawing.Size(250, 26);
+            this.nameInput.Size = new System.Drawing.Size(250, 29);
             this.nameInput.TabIndex = 1;
             // 
             // difficultyLabel
             // 
-            this.difficultyLabel.Font = new System.Drawing.Font("Press Start 2P", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.difficultyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.difficultyLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.difficultyLabel.Location = new System.Drawing.Point(13, 118);
             this.difficultyLabel.Name = "difficultyLabel";
@@ -75,7 +77,8 @@
             // easyButton
             // 
             this.easyButton.BackColor = System.Drawing.Color.Black;
-            this.easyButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.easyButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.easyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.easyButton.ForeColor = System.Drawing.Color.White;
             this.easyButton.Location = new System.Drawing.Point(17, 263);
             this.easyButton.Name = "easyButton";
@@ -84,11 +87,14 @@
             this.easyButton.Text = "Easy";
             this.easyButton.UseVisualStyleBackColor = false;
             this.easyButton.Click += new System.EventHandler(this.easyButton_Click);
+            this.easyButton.MouseEnter += new System.EventHandler(this.easyButton_MouseEnter);
+            this.easyButton.MouseLeave += new System.EventHandler(this.easyButton_MouseLeave);
             // 
             // normalButton
             // 
             this.normalButton.BackColor = System.Drawing.Color.Black;
-            this.normalButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.normalButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.normalButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.normalButton.ForeColor = System.Drawing.Color.White;
             this.normalButton.Location = new System.Drawing.Point(214, 144);
             this.normalButton.Name = "normalButton";
@@ -96,11 +102,15 @@
             this.normalButton.TabIndex = 4;
             this.normalButton.Text = "Normal";
             this.normalButton.UseVisualStyleBackColor = false;
+            this.normalButton.Click += new System.EventHandler(this.normalButton_Click);
+            this.normalButton.MouseEnter += new System.EventHandler(this.normalButton_MouseEnter);
+            this.normalButton.MouseLeave += new System.EventHandler(this.normalButton_MouseLeave);
             // 
             // hardButton
             // 
             this.hardButton.BackColor = System.Drawing.Color.Black;
-            this.hardButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hardButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.hardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hardButton.ForeColor = System.Drawing.Color.White;
             this.hardButton.Location = new System.Drawing.Point(404, 263);
             this.hardButton.Name = "hardButton";
@@ -108,11 +118,15 @@
             this.hardButton.TabIndex = 5;
             this.hardButton.Text = "Hard";
             this.hardButton.UseVisualStyleBackColor = false;
+            this.hardButton.Click += new System.EventHandler(this.hardButton_Click);
+            this.hardButton.MouseEnter += new System.EventHandler(this.hardButton_MouseEnter);
+            this.hardButton.MouseLeave += new System.EventHandler(this.hardButton_MouseLeave);
             // 
             // masterButton
             // 
             this.masterButton.BackColor = System.Drawing.Color.Black;
-            this.masterButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.masterButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.masterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.masterButton.ForeColor = System.Drawing.Color.White;
             this.masterButton.Location = new System.Drawing.Point(214, 393);
             this.masterButton.Name = "masterButton";
@@ -120,11 +134,15 @@
             this.masterButton.TabIndex = 6;
             this.masterButton.Text = "Master";
             this.masterButton.UseVisualStyleBackColor = false;
+            this.masterButton.Click += new System.EventHandler(this.masterButton_Click);
+            this.masterButton.MouseEnter += new System.EventHandler(this.masterButton_MouseEnter);
+            this.masterButton.MouseLeave += new System.EventHandler(this.masterButton_MouseLeave);
             // 
             // beginButton
             // 
             this.beginButton.BackColor = System.Drawing.Color.Black;
-            this.beginButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beginButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.beginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.beginButton.ForeColor = System.Drawing.Color.White;
             this.beginButton.Location = new System.Drawing.Point(214, 263);
             this.beginButton.Name = "beginButton";
@@ -132,11 +150,15 @@
             this.beginButton.TabIndex = 7;
             this.beginButton.Text = "Begin";
             this.beginButton.UseVisualStyleBackColor = false;
+            this.beginButton.Click += new System.EventHandler(this.beginButton_Click);
+            this.beginButton.MouseEnter += new System.EventHandler(this.beginButton_MouseEnter);
+            this.beginButton.MouseLeave += new System.EventHandler(this.beginButton_MouseLeave);
             // 
             // bButton
             // 
             this.bButton.BackColor = System.Drawing.Color.Black;
-            this.bButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bButton.ForeColor = System.Drawing.Color.White;
             this.bButton.Location = new System.Drawing.Point(422, 450);
             this.bButton.Name = "bButton";
@@ -144,11 +166,15 @@
             this.bButton.TabIndex = 8;
             this.bButton.Text = "B";
             this.bButton.UseVisualStyleBackColor = false;
+            this.bButton.Click += new System.EventHandler(this.bButton_Click);
+            this.bButton.MouseEnter += new System.EventHandler(this.bButton_MouseEnter);
+            this.bButton.MouseLeave += new System.EventHandler(this.bButton_MouseLeave);
             // 
             // aButton
             // 
             this.aButton.BackColor = System.Drawing.Color.Black;
-            this.aButton.Font = new System.Drawing.Font("Press Start 2P", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.aButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.aButton.ForeColor = System.Drawing.Color.White;
             this.aButton.Location = new System.Drawing.Point(-3, 450);
             this.aButton.Name = "aButton";
@@ -156,18 +182,25 @@
             this.aButton.TabIndex = 9;
             this.aButton.Text = "A";
             this.aButton.UseVisualStyleBackColor = false;
+            this.aButton.Click += new System.EventHandler(this.aButton_Click);
+            this.aButton.MouseEnter += new System.EventHandler(this.aButton_MouseEnter);
+            this.aButton.MouseLeave += new System.EventHandler(this.aButton_MouseLeave);
             // 
             // welcomeLabel
             // 
-            this.welcomeLabel.Font = new System.Drawing.Font("Press Start 2P", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.welcomeLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.welcomeLabel.Location = new System.Drawing.Point(121, 479);
             this.welcomeLabel.Name = "welcomeLabel";
             this.welcomeLabel.Size = new System.Drawing.Size(295, 23);
             this.welcomeLabel.TabIndex = 10;
-            this.welcomeLabel.Text = "Welcome";
+            this.welcomeLabel.Text = "Welcome To The Land Of Yore!";
             this.welcomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.welcomeLabel.Visible = false;
+            // 
+            // nameChecker
+            // 
+            this.nameChecker.Interval = 250;
+            this.nameChecker.Tick += new System.EventHandler(this.nameChecker_Tick);
             // 
             // Form1
             // 
@@ -207,6 +240,7 @@
         private System.Windows.Forms.Button bButton;
         private System.Windows.Forms.Button aButton;
         private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Timer nameChecker;
     }
 }
 
--- DifficultySettings/Form1.cs
+++ DifficultySettings/Form1.cs
@@ -13,9 +13,18 @@ namespace DifficultySettings
     public partial class Form1 : Form
     {
         int babyCounter = 0;
+        bool easyOn = false;
+        bool normalOn = false;
+        bool hardOn = false;
+        bool masterOn = false;
+        string[] directions = new string[1023];
+        int konamiOp = 0;
+        string[] konami = { "up", "up", "down", "down", "left", "right", "left", "right", "B", "A", "begin"};
+        int index = 0;
         public Form1()
         {
             InitializeComponent();
+            nameChecker.Start();
         }
 
         private void easyButton_Click(object sender, EventArgs e)
@@ -40,7 +49,15 @@ namespace DifficultySettings
                 setToBlack();
                 easyButton.BackColor = Color.Green;
                 difficultyLabel.Text = "Really?";
-                babyCounter++;
+                babyCounter++; 
+
+                easyOn = true;
+                normalOn = false;
+                hardOn = false;
+                masterOn = false;
+
+                directions[index] = "left";
+                index++;
             }
         }
         void setToBlack()
@@ -51,6 +68,221 @@ namespace DifficultySettings
             masterButton.BackColor = Color.Black;
             aButton.BackColor = Color.Black;
             bButton.BackColor = Color.Black;
+        }
+
+        void indexCheck()
+        {
+            konamiOp = 0;
+            int indexSub = 0;
+            for (int i = 0; i <= index; i++)
+            {
+                if (konamiOp == 11)
+                {
+                    difficultyLabel.Text = "!Cheater!";
+                    easyButton.BackgroundImage = Properties.Resources.konamiImage;
+                    normalButton.BackgroundImage = Properties.Resources.konamiImage;
+                    hardButton.BackgroundImage = Properties.Resources.konamiImage;
+                    masterButton.BackgroundImage = Properties.Resources.konamiImage;
+                    beginButton.BackgroundImage = Properties.Resources.konamiImage;
+                    aButton.BackgroundImage = Properties.Resources.konamiImage;
+                    bButton.BackgroundImage = Properties.Resources.konamiImage;
+                    easyButton.Text = "";
+                    normalButton.Text = "";
+                    hardButton.Text = "";
+                    masterButton.Text = "";
+                    beginButton.Text = "";
+                    aButton.Text = "";
+                    bButton.Text = "";
+                }
+                else if (directions[indexSub + i] == konami[konamiOp])
+                {
+                    konamiOp++;
+                }
+            }
+        }
+
+        private void easyButton_MouseEnter(object sender, EventArgs e)
+        {
+            easyButton.BackColor = Color.LimeGreen;
+        }
+
+        private void easyButton_MouseLeave(object sender, EventArgs e)
+        {
+            if (easyOn == false)
+            {
+                easyButton.BackColor = Color.Black;
+            }
+            else
+            {
+                easyButton.BackColor = Color.Green;
+            }
+        }
+
+        private void aButton_MouseEnter(object sender, EventArgs e)
+        {
+            aButton.BackColor = Color.LightBlue;
+        }
+
+        private void aButton_MouseLeave(object sender, EventArgs e)
+        {
+            aButton.BackColor = Color.Black;
+        }
+
+        private void aButton_Click(object sender, EventArgs e)
+        {
+            difficultyLabel.Text = "Worry Not About Those Buttons! They are Useless";
+            directions[index] = "A";
+            index++;
+        }
+
+        private void nameChecker_Tick(object sender, EventArgs e)
+        {
+            if ((nameInput.Text == "") == false) 
+            {
+                string nameHold = Convert.ToString(nameInput.Text);
+                welcomeLabel.Text = $"Welcome {nameHold}, to the Land of Yore!";
+                welcomeLabel.Visible = true;
+            }
+        }
+
+        private void bButton_Click(object sender, EventArgs e)
+        {
+            difficultyLabel.Text = "Worry Not About Those Buttons! They are Useless";
+            directions[index] = "B";
+            index++;
+        }
+
+        private void bButton_MouseEnter(object sender, EventArgs e)
+        {
+            bButton.BackColor = Color.LightBlue;
+        }
+
+        private void bButton_MouseLeave(object sender, EventArgs e)
+        {
+            bButton.BackColor = Color.Black;
+        }
+
+        private void normalButton_Click(object sender, EventArgs e)
+        {
+            setToBlack();
+            normalButton.BackColor = Color.Yellow;
+            difficultyLabel.Text = "Respectable";
+
+            easyOn = false;
+            normalOn = true;
+            hardOn = false;
+            masterOn = false;
+
+            directions[index] = "up";
+            index++;
+        }
+
+        private void hardButton_Click(object sender, EventArgs e)
+        {
+            setToBlack();
+            hardButton.BackColor = Color.Orange;
+            difficultyLabel.Text = "Thou Shalt Be Challenged";
+
+            easyOn = false;
+            normalOn = false;
+            hardOn = true;
+            masterOn = false;
+
+            directions[index] = "right";
+            index++;
+        }
+
+        private void masterButton_Click(object sender, EventArgs e)
+        {
+            setToBlack();
+            masterButton.BackColor = Color.Red;
+            difficultyLabel.Text = "Art Thou Insane?!";
+
+            easyOn = false;
+            normalOn = false;
+            hardOn = false;
+            masterOn = true;
+
+            directions[index] = "down";
+            index++;
+        }
+
+        private void beginButton_Click(object sender, EventArgs e)
+        {
+            setToBlack();
+            beginButton.BackColor = Color.Gold;
+            difficultyLabel.Text = "Beginning your Quest";
+
+            easyOn = false;
+            normalOn = false;
+            hardOn = false;
+            masterOn = false;
+
+            directions[index] = "begin";
+            index++;
+            indexCheck();
+
+        }
+
+        private void normalButton_MouseEnter(object sender, EventArgs e)
+        {
+            normalButton.BackColor = Color.YellowGreen;
+        }
+
+        private void normalButton_MouseLeave(object sender, EventArgs e)
+        {
+            if (normalOn == false)
+            {
+                normalButton.BackColor = Color.Black;
+            }
+            else
+            {
+                normalButton.BackColor = Color.Yellow;
+            }
+        }
+
+        private void hardButton_MouseEnter(object sender, EventArgs e)
+        {
+            hardButton.BackColor = Color.OrangeRed;
+        }
+
+        private void hardButton_MouseLeave(object sender, EventArgs e)
+        {
+            if (hardOn == false)
+            {
+                hardButton.BackColor = Color.Black;
+            }
+            else
+            {
+                hardButton.BackColor = Color.Orange;
+            }
+        }
+
+        private void masterButton_MouseEnter(object sender, EventArgs e)
+        {
+            masterButton.BackColor = Color.DarkRed;
+        }
+
+        private void masterButton_MouseLeave(object sender, EventArgs e)
+        {
+            if (masterOn == false)
+            {
+                masterButton.BackColor = Color.Black;
+            }
+            else
+            {
+                masterButton.BackColor = Color.Red;
+            }
+        }
+
+        private void beginButton_MouseEnter(object sender, EventArgs e)
+        {
+            beginButton.BackColor = Color.Silver;
+        }
+
+        private void beginButton_MouseLeave(object sender, EventArgs e)
+        {
+                beginButton.BackColor = Color.Black;
         }
     }
 }
